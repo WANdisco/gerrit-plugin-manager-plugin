GerritCodeReview/plugins_plugin-manager

Remove unnecessarily nested else clauses
Change-Id: I281d139b00325419362bc4011462676f25a15a11

src/main/java/com/googlesource/gerrit/plugins/manager/gson/SmartJson.java
@@ -52,9 +52,8 @@ public class SmartJson {
     if (jsonElem != null && jsonElem.getAsJsonObject().get(fieldName) != null) {
       return Optional.of(SmartJson
           .of(jsonElem.getAsJsonObject().get(fieldName)));
-    } else {
-      return Optional.empty();
     }
+    return Optional.empty();
   }
 
   public SmartJson get(String fieldName) {

WANdisco/gerrit-plugin-manager-plugin

src/main/java/com/googlesource/gerrit/plugins/manager/gson/SmartJson.java
@@ -51,9 +51,8 @@ public class SmartJson {
     if (jsonElem != null && jsonElem.getAsJsonObject().get(fieldName) != null) {
       return Optional.of(SmartJson
           .of(jsonElem.getAsJsonObject().get(fieldName)));
-    } else {
-      return Optional.absent();
     }
+    return Optional.absent();
   }
 
   public SmartJson get(String fieldName) {